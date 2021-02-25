2e7923865fd832e732f0fade600fd608f12de705
yuquanf <420125423@qq.com>
2021-02-25 19:22:43 +0800

    refactor: 重做404页面

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,18 +1,19 @@
 <template>
-  <div id="page-404">
-    <section class="error-container">
-      <span class="four"><span class="screen-reader-text">4</span></span>
-      <span class="zero"><span class="screen-reader-text">0</span></span>
-      <span class="four"><span class="screen-reader-text">4</span></span>
-    </section>
-    <div class="link-container">
-      <router-link
-        to="/"
-      >
-        <a-button type="primary">
-          {{ $l('backToHomepage') }}
-        </a-button>
-      </router-link>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img :src="img404" alt="404" />
+      </div>
+      <div class="col">
+        <h1>404</h1>
+        <h2>UH OH! 页面丢失</h2>
+        <p>您所寻找的页面不存在。你可以点击下面的按钮，返回主页。</p>
+        <router-link to="/">
+          <a-button type="primary">
+            {{ $l('backToHomepage') }}
+          </a-button>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -20,11 +21,28 @@
 <script>
 export default {
   name: 'Page404',
-  mounted () {
+  data() {
+    return {
+      img404: require('../assets/post/404.svg'),
+    }
+  },
+  mounted() {
     document.title = `${this.$l('404')} - ${this.$l('title')}`
   },
 }
 </script>
 
 <style lang="less" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - 84px);
+}
+.row {
+  display: flex;
+}
+.col {
+  min-width: 500px;
+}
 </style>
